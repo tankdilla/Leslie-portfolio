9826e33628350e2a8ebf800eace0c33c37373079
darrell <darrellj@gmail.com>
2011-02-26 21:40:28 -0600

    minor tweaks to the site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,11 @@ class WelcomeController < ApplicationController
   end
   
   def resume
-  	send_file 'public/resources/leslie_johnson_resume.pdf', :disposition => 'inline',:type => 'application/pdf'
+  	send_file "#{RAILS_ROOT}/public/resources/leslie_johnson_resume.pdf", :disposition => 'inline',:type => 'application/pdf'
   end
   
   def samples
-  	send_file 'public/resources/leslie_johnson_samples.pdf', :disposition => 'inline',:type => 'application/pdf'
+  	send_file "#{RAILS_ROOT}/public/resources/leslie_johnson_samples.pdf", :disposition => 'inline',:type => 'application/pdf'
   end
 
 end
